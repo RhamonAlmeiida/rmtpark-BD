c0c09c31b786c16ec4b45694d09e55295ebe7a81
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-09-27 01:45:50 -0300

    alterado banco de dados

--- src/rmtpark_api/database/banco_dados.py
+++ src/rmtpark_api/database/banco_dados.py
@@ -15,7 +15,8 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 # Cria engine
 engine = create_engine(
