2cffa3746d27988c2e8e33790ea6b3f8bc9c7d96
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-09-27 17:16:10 -0300

    alterado url front para vercel

--- src/rmtpark_api/main.py
+++ src/rmtpark_api/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="RmtPark API")
 origins = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-    "https://rmtpark.netlify.app",
+    "https://rmtpark-tcc-u856.vercel.app",
     "https://api.rmt-park.com"
 ]
 
