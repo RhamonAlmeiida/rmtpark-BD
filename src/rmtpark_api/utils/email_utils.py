ae26ba2af7c94f92329ee54fb60ae3327a43f5b2
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-09-28 00:16:07 -0300

    utilits

--- src/rmtpark_api/utils/email_utils.py
+++ src/rmtpark_api/utils/email_utils.py
@@ -2,23 +2,21 @@
 from dotenv import load_dotenv
 load_dotenv()  # Carrega o .env antes de pegar as variáveis
 
-import os
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+import os
 
-# -----------------------
-# Configuração do FastMail pronta para produção
-# -----------------------
 conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
     MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS=os.getenv("MAIL_STARTTLS", "True") == "True",
-    MAIL_SSL_TLS=os.getenv("MAIL_SSL_TLS", "False") == "True",
+    MAIL_TLS=True,        # substitui MAIL_STARTTLS
+    MAIL_SSL=False,       # substitui MAIL_SSL_TLS
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
 )
+
 
 
 # URL do frontend
