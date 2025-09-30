58450833230d4b4ccb65d0aa039175826e6412e7
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-09-30 16:44:29 -0300

    teste correção erro cadastro

--- src/rmtpark_api/api/empresa.py
+++ src/rmtpark_api/api/empresa.py
@@ -10,35 +10,32 @@ router = APIRouter(prefix="/empresas", tags=["empresas"])
 cnpj_validator = CNPJ()
 
 
+from sqlalchemy.exc import IntegrityError
+
 @router.post("/", response_model=EmpresaOut)
 def criar_empresa(empresa: EmpresaCreate, db: Session = Depends(banco_dados.get_db)):
-
     if not cnpj_validator.validate(empresa.cnpj):
         raise HTTPException(status_code=400, detail="CNPJ inválido")
 
+    try:
+        hashed_senha = hash_password(empresa.senha)
 
-    if db.query(Empresa).filter(Empresa.email == empresa.email).first():
-        raise HTTPException(status_code=400, detail="Email já cadastrado")
+        nova_empresa = Empresa(
+            nome=empresa.nome,
+            email=empresa.email,
+            telefone=empresa.telefone,
+            cnpj=empresa.cnpj,
+            senha=hashed_senha,
+            email_confirmado=False,
+        )
 
+        db.add(nova_empresa)
+        db.commit()
+        db.refresh(nova_empresa)
 
-    if db.query(Empresa).filter(Empresa.cnpj == empresa.cnpj).first():
-        raise HTTPException(status_code=400, detail="CNPJ já cadastrado")
+        return EmpresaOut.model_validate(nova_empresa)
 
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Email ou CNPJ já cadastrado")
 
-    hashed_senha = hash_password(empresa.senha)
-
-    nova_empresa = Empresa(
-        nome=empresa.nome,
-        email=empresa.email,
-        telefone=empresa.telefone,
-        cnpj=empresa.cnpj,
-        senha=hashed_senha,
-        email_confirmado=False,
-    )
-
-    db.add(nova_empresa)
-    db.commit()
-    db.refresh(nova_empresa)
-
-
-    return nova_empresa
