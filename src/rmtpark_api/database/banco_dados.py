8ca21a638ade4632cf43ba0c1681634e05d526c1
Rhamon Silva Almeida <75868@PROWAY.TREINA>
2025-09-30 21:42:10 -0300

    Ajustado SSL do banco

--- src/rmtpark_api/database/banco_dados.py
+++ src/rmtpark_api/database/banco_dados.py
@@ -12,9 +12,10 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", 5432)
 DB_NAME = os.getenv("DB_NAME")
+DB_SSL_MODE = os.getenv("DB_SSL_MODE", "disable")
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=disable"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSL_MODE}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
