ca74cd1bebff0518b6ca554898ec8683f011b748
Rhamon Silva Almeida <rhamonalmeida.dev@hotmail.com>
2025-10-03 19:59:09 -0300

    alterado ordem configurações 2

--- src/rmtpark_api/main.py
+++ src/rmtpark_api/main.py
@@ -1,57 +1,44 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.rmtpark_api.database.banco_dados import Base, engine
-from src.rmtpark_api.api import auth, empresa, vaga, relatorio, mensalista
-from src.rmtpark_api.api import teste_email
+from src.rmtpark_api.api import auth, empresa, vaga, relatorio, mensalista, teste_email
+import os
+
 app = FastAPI(title="RmtPark API")
 
-# Cria todas as tabelas
+# Cria todas as tabelas no banco
 Base.metadata.create_all(bind=engine)
-
 
 # CORS
 origins = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
     "https://rmtpark-tcc-u856.vercel.app",
-    "https://rmtpark-api.onrender.com",
+    "https://rmtpark-bd.onrender.com",
 ]
-
-
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:4200",
-        "http://127.0.0.1:4200",
-        "https://rmtpark-tcc-u856.vercel.app",
-        "https://rmtpark-bd.onrender.com",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-),
+)
 
-
-# Routers
+# Rotas
 app.include_router(auth.router, prefix="/api/auth")
 app.include_router(empresa.router, prefix="/api/empresa")
 app.include_router(vaga.router, prefix="/api/vagas", tags=["vagas"])
 app.include_router(relatorio.router, prefix="/api/relatorios", tags=["relatorios"])
 app.include_router(mensalista.router, prefix="/api/mensalistas", tags=["mensalistas"])
 app.include_router(teste_email.router, prefix="/api")
+
 @app.get("/")
 def home():
     return {"status": "API RmtPark funcionando 🚀"}
 
-# Executa local
-import os
-import uvicorn
-
+# Executa apenas se rodar localmente
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("src.rmtpark_api.main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Render usa a variável PORT
+    import uvicorn
+    uvicorn.run("src.rmtpark_api.main:app", host="0.0.0.0", port=port)
