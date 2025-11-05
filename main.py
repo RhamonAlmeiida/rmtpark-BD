903b30daa2c5c4538a81fe30c66921cb67887fdd
Rhamon Almeida <rhamonalmeida@MacBook-Air-de-Rhamon.local>
2025-11-05 02:40:13 -0300

    alterado main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-app = FastAPI(title="RmtPark API", version="1.0.0",redirect_slashes=False)
+app = FastAPI(title="RmtPark API", version="1.0.0")
 
 # 🔹 Mover o bloco CORS imediatamente após criar o app
 origins = [
